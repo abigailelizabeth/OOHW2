42dab5f3e32445aabd8dc0391b6799d0705b65a7
Jalufka <jrj105@txstate.edu>
2016-10-11 13:42:42 -0500

    test commit

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -8,6 +8,7 @@ public class TestDriver {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hi");
 		LibrarySystem alkek = LibrarySystem.getInstance();
 
 	}
