337d2d1b677288ce86605604587ca52f301046a6
Abigail Barron <abbyeb22@gmail.com>
2016-10-12 12:13:02 -0500

    implemented Document Class, added author class

--- src/edu/txstate/library/Document.java
+++ src/edu/txstate/library/Document.java
@@ -1,5 +1,55 @@
 package edu.txstate.library;
 
 public abstract class Document {
+	private String mTitle;
+	private String mPublishDate;
+	private String mPublisher;
+	private int mNumberOfCopies;
+	
+	
+	// Constructor
+	public Document(){
+	
+	}
+	
+	// All the Setters
+	public void setTitle(String title){
+		this.mTitle = title;
+	}
+	public void setPublishDate(String publishDate){
+		this.mPublishDate = publishDate;
+	}
+	public void setPublisher(String publisher){
+		this.mPublisher = publisher;
+	}
+	public void setNumberOfCopies(int num){
+		this.mNumberOfCopies = num;
+	}
+	
+	//All the Getters
+	public String getTitle(){
+		return this.mTitle;
+	}
+	public String getPublishDate(){
+		return this.mPublishDate;
+	}
+	public String getPublisher(){
+		return this.mPublisher;
+	}
+	public int getNumberOfCopies(){
+		return this.mNumberOfCopies;
+	}
+	public int getNumberOfAuthors(){
+		return Author.getNumOfInstances();
+	}
+	
+	// update mNumberOfCopies
+	public void incrementNumberOfCopies(){
+		this.mNumberOfCopies = this.mNumberOfCopies + 1;
+	}
+	public void deccrementNumberOfCopies(){
+		this.mNumberOfCopies = this.mNumberOfCopies - 1;
+	}
+	
 
 }
