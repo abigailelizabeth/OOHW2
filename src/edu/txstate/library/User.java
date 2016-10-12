df7d3cb9bbf5ec8e2154746c56bcfaf639951391
Jason Jalufka <jasonjalufka@gmail.com>
2016-10-12 13:49:25 -0500

    Updated Jason's classes

--- src/edu/txstate/library/User.java
+++ src/edu/txstate/library/User.java
@@ -1,6 +1,18 @@
 package edu.txstate.library;
 
 public abstract class User {
+	private String mUserId;
 	
+	public User(){
+		
+	}
+	
+	public void setUserId(String userId){
+		this.mUserId = userId;
+	}
+	
+	public String getUserId(){
+		return this.mUserId;
+	}
 
 }
